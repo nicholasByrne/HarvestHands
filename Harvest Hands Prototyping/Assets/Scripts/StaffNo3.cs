8630dfc6fe968cdc010c8f48d9c0432e0f0f09bd
Nicholas Byrne <nicholas.byrne@ad.aie.edu>
2016-07-25 16:24:28 +1000

    Added reworked staff and bucket and water

---
+++ Harvest Hands Prototyping/Assets/Scripts/StaffNo3.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaffNo3 : MonoBehaviour
+{
+
+
+
+
+    //game objects you can pick up
+    //add them here first
+    GameObject Bucket;
+    GameObject TestItem;
+
+
+    GameObject WaterPuddle;
+
+    bool BucketFill;
+    bool PuddleEmpty;
+
+    RaycastHit Hit;
+
+    GameObject BucketWater;
+    GameObject StaffGrabber;
+
+    Vector3 camcent;
+    Vector3 lastPos;
+    Vector3 lastPosRot;
+
+    bool objectheld;
+
+   
+    float timeLeft;
+
+
+
+    GameObject ChosenObj;
+    Camera m_Camera;
+
+    public float throwforce = 500f;
+
+    public float GrabDistance = 3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        //initialize the gameobjects here
+        Bucket = GameObject.Find("Bucket");
+        TestItem = GameObject.Find("TestItem");
+
+
+
+
+
+        BucketFill = false;
+        WaterPuddle = GameObject.Find("Puddle");
+        BucketWater = GameObject.Find("BucketWater");
+        PuddleEmpty = false;
+        StaffGrabber = GameObject.Find("Staff Grabber");
+
+
+
+        objectheld = false;
+      
+        timeLeft = 0.02f;
+
+
+        lastPos = ChosenObj.transform.position;
+        lastPosRot = ChosenObj.transform.rotation.eulerAngles;
+
+       // throwforce = 500f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        if (objectheld == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+
+                Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+
+                if (Physics.Raycast(ray, out Hit, GrabDistance))
+                {
+
+                    ChosenObj = Hit.collider.gameObject;
+
+
+
+                    if ((Hit.collider.gameObject.GetComponent<Pickupable>() != null))
+                    {
+                        Debug.Log("SUCESS");
+
+
+
+                        ChosenObj = Hit.collider.gameObject;
+
+
+                        objectheld = true;
+                        ChosenObj.transform.parent = StaffGrabber.transform;
+                        ChosenObj.GetComponent<Rigidbody>().useGravity = false;
+
+
+                    }
+                }
+            }
+        }
+        else
+        {
+            ChosenObj.GetComponent<Rigidbody>().MovePosition(StaffGrabber.transform.position);
+
+
+            ChosenObj.GetComponent<Rigidbody>().useGravity = false;
+            ChosenObj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            ChosenObj.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+            timeLeft -= Time.deltaTime;
+
+            //if bucket held , throws the bucket
+            if (Input.GetMouseButtonDown(0))
+            {
+              
+
+                ChosenObj.GetComponent<Rigidbody>().useGravity = true;
+                ChosenObj.transform.parent = null;
+
+
+                ChosenObj.GetComponent<Rigidbody>().AddForce(transform.forward * throwforce); 
+
+
+                objectheld = false;
+            }
+        }
+
+
+
+        Vector3 offset = ChosenObj.transform.position - lastPos;
+        Vector3 offsetrot = ChosenObj.transform.rotation.eulerAngles - lastPosRot;
+
+
+
+        StaffGrabber.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane + 4));
+
+        //moveposition
+
+
+        if (Input.GetMouseButton(1))
+        {
+
+
+            //this drops the bucket 
+            ChosenObj.GetComponent<Rigidbody>().useGravity = true;
+            ChosenObj.transform.parent = null;
+            objectheld = false;
+        }
+
+    }
+
+
+
+    void OnCollisionEnter(Collision coll)
+    {   //if bucket colides with something push it back to the grab location
+        ChosenObj.transform.position = Vector3.MoveTowards(transform.position, StaffGrabber.transform.position, 1);
+    }
+
+
+}
--- Harvest Hands Prototyping/Assets/Scripts/Water.cs
+++ Harvest Hands Prototyping/Assets/Scripts/Water.cs
@@ -18,19 +18,116 @@ public class Water : MonoBehaviour {
 
     bool bucketheld;
 
+    bool watertoggle;
+
+    float waterlevel;
+
     // Use this for initialization
     void Start () {
        
         BucketFill = false;
         WaterPuddle = GameObject.Find("Puddle");
-        BucketWater = GameObject.Find("BucketWater");
+        BucketWater = GameObject.Find("WaterInBucket");
         PuddleEmpty = false;     
         Bucket = GameObject.Find("Bucket");
 
+        watertoggle = false;
+        waterlevel = 0.0f;
+
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        //BucketWater.transform.position = Bucket.transform.position;
+
+
+        if (watertoggle == false)
+        {
+            BucketWater.SetActive(false);
+        
+        }
+
+
+        if (watertoggle == true)
+        {
+            BucketWater.SetActive(true);
+        }
+
+
+
+
+
+
+     //   if (Bucket)
+        if (waterlevel > 0)
+        {
+            watertoggle = true;
+        
+        }
+
+
+
+        if (waterlevel <= 0)
+        {
+            watertoggle = false;
+
+        }
+
+
+
+        if (waterlevel > 0 && waterlevel <= 1)
+        {
+
+            Vector3 tmpPos = BucketWater.transform.localPosition; // Store all Vector3
+            tmpPos.y = 0.2f; // example assign individual fox Y axe
+            BucketWater.transform.localPosition = tmpPos; // Assign back all Vector3
+
+
+          //  BucketWater.transform.position.Set(BucketWater.transform.position.x, 0.3f, BucketWater.transform.position.z);
+        }
+
+
+        if (waterlevel > 1 && waterlevel <= 2)
+        {
+
+
+            Vector3 tmpPos = BucketWater.transform.localPosition; // Store all Vector3
+            tmpPos.y = 0.5f; // example assign individual fox Y axe
+            BucketWater.transform.localPosition = tmpPos; // Assign back all Vector3
+
+
+           // BucketWater.transform.position.Set(BucketWater.transform.position.x,0.5f,BucketWater.transform.position.z);
+        }
+
+
+        if (waterlevel > 2 && waterlevel <= 3)
+        {
+
+
+            
+
+            Vector3 tmpPos = BucketWater.transform.localPosition; // Store all Vector3
+            tmpPos.y = 0.7f; // example assign individual fox Y axe
+            BucketWater.transform.localPosition = tmpPos; // Assign back all Vector3
+
+
+            //BucketWater.transform.position.Set(BucketWater.transform.position.x, 0.7f, BucketWater.transform.position.z);
+
+
+           
+        }
+
+
+
+
+        if (Input.GetMouseButtonDown(2) && waterlevel > 0)
+        {
+            waterlevel -= 0.5f;
+        }
+
+
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -74,6 +171,23 @@ public class Water : MonoBehaviour {
             }
 
         }
+
+
+    }
+
+
+
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "Water")
+        {
+            waterlevel = 3.0f;
+        
+        
+        }
+
+
 
     }
 
